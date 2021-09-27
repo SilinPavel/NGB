4121b5d6d739f4ab0ec2f275aebfd2a658f783f8
Aleksei_Golovenchik <aleksei_golovenchik@epam.com>
2021-09-27 12:08:25 +0300

    adds to MotifSearchManagerTest tests with the shortest regex

--- server/catgenome/src/test/java/com/epam/catgenome/manager/reference/MotifSearchManagerTest.java
+++ server/catgenome/src/test/java/com/epam/catgenome/manager/reference/MotifSearchManagerTest.java
@@ -647,4 +647,34 @@ public class MotifSearchManagerTest {
         Assert.assertNull(search.getPosition());
         Assert.assertFalse(search.getResult().isEmpty());
     }
+
+    @Test(expected = Test.None.class)
+    @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
+    public void shouldNotFailWithOutOfMemoryErrorWhenWeSearchShortestMotif() {
+        MotifSearchRequest att = MotifSearchRequest.builder()
+                .referenceId(testReference.getId())
+                .startPosition(1)
+                .chromosomeId(testChromosome.getId())
+                .motif("[acgtn]")
+                .searchType(MotifSearchType.WHOLE_GENOME)
+                .pageSize(PAGE_SIZE)
+                .strand(null)
+                .build();
+        motifSearchManager.search(att);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
+    public void shouldFailWhenWeSearchShortestRegexMotif() {
+        MotifSearchRequest att = MotifSearchRequest.builder()
+                .referenceId(testReference.getId())
+                .startPosition(1)
+                .chromosomeId(testChromosome.getId())
+                .motif(".")
+                .searchType(MotifSearchType.WHOLE_GENOME)
+                .pageSize(PAGE_SIZE)
+                .strand(StrandSerializable.POSITIVE)
+                .build();
+        motifSearchManager.search(att);
+    }
 }
